303be13d210840aea1b139e01765852801ea7cf7
iamthepoe <lucagamerbrime@gmail.com>
2023-04-03 19:01:00 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using enquete_asp.Models;
+
+namespace enquete_asp.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Responder()
+    {
+        return View();
+    }
+
+}
---
+++ Models/RespostaModel.cs
@@ -0,0 +1,8 @@
+namespace enquete_asp.Models;
+
+public class RespostaModel
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public bool? Sim { get; set; }
+}
