32bf41ae68d2d41d46e6c8ef5a51a3c26aca45a2
iamthepoe <lucagamerbrime@gmail.com>
2023-04-06 19:14:16 -0300

    add new validations

--- Models/RespostaModel.cs
+++ Models/RespostaModel.cs
@@ -4,12 +4,16 @@ namespace enquete_asp.Models;
 
 public class RespostaModel
 {
-    [Required]
+
+    [Required(ErrorMessage = "O campo 'Nome' deve estar preenchido.")]
     public string Nome { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "O campo 'Email' deve estar preenchido.")]
+
+    [EmailAddress(ErrorMessage = "O endereço de email é inválido")]
     public string Email { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "O campo de resposta deve estar preenchido.")]
     public bool? Sim { get; set; }
+
 }
